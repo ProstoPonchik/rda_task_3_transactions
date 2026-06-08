2906e8095c238273dc56c06946094f5dccc69c4a
ProstoPonchik <vlad.vislovukh@gmail.com>
2026-06-09 00:08:07 +0200

    Fixed 2.0!

--- task.sql
+++ task.sql
@@ -1,20 +1,19 @@
 -- Use our database
-USE ShopDB; 
+USE ShopDB;
 
 -- ShopDB should be created by running create-database.sql before this script.
 -- AwersomeProduct with ID 1 and customer with ID 1 are inserted there as initial data.
 
--- Start the transaction
-START TRANSACTION; 
-
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
+
+-- Start the transaction
+START TRANSACTION;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (1, 1, 1);
 
-UPDATE Products
-SET WarehouseAmount = WarehouseAmount - 1
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1
 WHERE ID = 1;
 
-COMMIT; 
+COMMIT;
